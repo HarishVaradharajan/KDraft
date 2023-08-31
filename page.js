7b36c6650de745d99f4cfa23df23444dc731e133
Harish <141133086+HarishVaradharajan@users.noreply.github.com>
2023-08-31 16:50:56 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+        #para1 {
+            text-align: center;
+            color:black;
+            font-size: 60px;
+        }
+        /* Basic styling for the images */
+        img {
+            width: 1250px;
+            height: 500px;
+            cursor: pointer;
+            margin: 20px;
+        }
